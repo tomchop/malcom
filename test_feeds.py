542bf5b8d9f97da21ad2013e168594be12c57e05
Thomas Chopitea <tomchop@gmail.com>
2015-11-13 14:23:25 +0100

    Changed feed tester

--- test_feeds.py
+++ test_feeds.py
@@ -1,4 +1,4 @@
-from Malcom.feeds.feed import FeedEngine
+from Malcom.feeds.core import FeedEngine
 import os
 
 all_feeds = [
@@ -20,21 +20,18 @@ feed_dir = os.getcwd()+'/Malcom/feeds'
 print "Testing feeds in", feed_dir
 fe = FeedEngine({'FEEDS_DIR': feed_dir})
 
-fe.load_feeds([f.lower() for f in all_feeds])
+fe.load_feeds(feed_dir)
 
 results = {}
 
 for feed in fe.feeds:
-	print "Testing feed %s" % feed
-	fe.feeds[feed].testing = True
-	fe.feeds[feed].update()
-	print "Test on %s succeeeded (%s elements fetched)" % (feed, fe.feeds[feed].elements_fetched)
-	results[feed] = fe.feeds[feed].elements_fetched
+	if feed in tested_feeds:
+		print "Testing feed %s" % feed
+		fe.feeds[feed].testing = True
+		fe.feeds[feed].update()
+		print "Test on %s succeeeded (%s elements fetched)" % (feed, fe.feeds[feed].elements_fetched)
+		results[feed] = fe.feeds[feed].elements_fetched
 
 print "Test results:"
 for f in results:
 	print "{0: <32} {1:8}".format(f, results[f])
-
-
-
-
